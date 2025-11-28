3537bff6a9170867885ae515e1cd25d5f558ad08
LucasAC01 <lucas@ambercreative.sg>
2025-11-28 10:19:13 +0800

    Added power function back.

--- calculator.py
+++ calculator.py
@@ -32,3 +32,10 @@ def divide(a, b):
     if DEBUG:
         print(f"DEBUG: {a} ÷ {b} = {result}")
     return result
+
+def power(a, b):
+    """Return the power of two numbers."""
+    result = a ** b
+    if DEBUG:
+        print(f"DEBUG: {a} ** {b} = {result}")
+    return result
